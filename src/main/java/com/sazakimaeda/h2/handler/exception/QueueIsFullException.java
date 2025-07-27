c2d7fa2ae88c766acc86579a78ee2b8d2cc77da1
SazakiMaeda <sazakimaeda@gmail.com>
2025-07-27 17:23:29 +0300

    android

--- src/main/java/com/sazakimaeda/h2/handler/exception/QueueIsFullException.java
+++
@@ -1,7 +0,0 @@
-package com.sazakimaeda.h2.handler.exception;
-
-public class QueueIsFullException extends RuntimeException{
-    public QueueIsFullException(){
-        super("Queue is full");
-    }
-}
--- src/main/java/com/sazakimaeda/h2/service/AuditService.java
+++
@@ -1,7 +0,0 @@
-package com.sazakimaeda.h2.service;
-
-public interface AuditService {
-
-    void send(String audit);
-
-}
